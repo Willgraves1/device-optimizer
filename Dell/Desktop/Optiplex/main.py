41223d69a9967808e8620e83380456924391ef87
Willgraves1 <118127287+Willgraves1@users.noreply.github.com>
2023-12-08 13:14:00 +0000

    edit

---
+++ Dell/Desktop/Optiplex/main.py
@@ -0,0 +1,10 @@
+import platform
+import subprocess
+
+if '64bit' in platform.architecture():
+    process = '%systemroot%\Sysnative\pnputil.exe -e'
+else:
+    process = 'pnputil.exe -e'
+
+p = subprocess.Popen(process, shell=True,
+        stdout=subprocess.PIPE, universal_newlines=True)
